8c426b87cd732d2f1a7b5fe137a189a2f7ac0f32
shaked_arazi <shaked13579@gmail.com>
2025-05-13 22:06:43 +0300

    Flask base app working with index route and venv setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,11 @@
- 
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'dev'  # נחליף אותו במשתנה סביבה בהמשך
+
+    from .routes import main
+    app.register_blueprint(main)
+
+    return app
+
